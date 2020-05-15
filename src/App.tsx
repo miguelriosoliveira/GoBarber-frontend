70b7410f0ff8b52de4281c5b796b26d5fdac4731
Miguel Rios <miguel@adianta.com.br>
2020-05-14 23:34:28 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-	return <h1>app</h1>;
+	return (
+		<>
+			<GlobalStyle />
+			<h1>app</h1>
+		</>
+	);
 };
 
 export default App;
